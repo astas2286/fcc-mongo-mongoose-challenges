36a9d866cd25ed65d7d4133083659a022d27a12c
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2022-06-08 17:47:22 +0300

    Add remove one

--- myApp.js
+++ myApp.js
@@ -129,7 +129,13 @@ const findAndUpdate = (personName, done) => {
 }
 
 const removeById = (personId, done) => {
-  done(null /*, data*/ );
+  Person.findByIdAndRemove(personId, function(err, data){
+    if (err) {
+      return console.log(err);
+    } else {
+      done(null, data)
+    };
+  });
 };
 
 const removeManyPeople = (done) => {
