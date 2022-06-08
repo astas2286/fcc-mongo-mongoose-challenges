4ab4ca6287e60138a0c49105565037729a9b92b2
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2022-06-08 17:55:41 +0300

    Add removeManyPeople

--- myApp.js
+++ myApp.js
@@ -140,8 +140,13 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/ );
+  Person.remove({name: nameToRemove}, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      done(null, data);
+    }
+  })
 };
 
 const queryChain = (done) => {
